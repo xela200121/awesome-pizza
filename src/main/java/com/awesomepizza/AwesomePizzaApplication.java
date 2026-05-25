861a8654cf046760163c25974577d70c754995c5
Alessio Aprile <hhalessio@gmail.com>
2026-05-25 17:00:20 +0200

    initialize Spring Boot project

---
+++ src/main/java/com/awesomepizza/AwesomePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.awesomepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomePizzaApplication.class, args);
+	}
+
+}
